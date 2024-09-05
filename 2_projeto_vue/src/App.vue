53a81650dcc20a5a051dda13dbc327bef0fc25ee
Pedro Isac <peedroisac@gmail.com>
2024-09-04 23:05:46 -0300

    novo ws

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,6 +1,8 @@
 <template>
+  <the-header />
   <div>
     <PrimeiroComponente />
+    <p class="paragrafo-pai">O dado de app é: {{testando}}</p>
     <LifeCycle />
     <Pessoa />
   </div>
@@ -10,13 +12,36 @@
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 import LifeCycle from './components/LifeCycle.vue';
 import Pessoa from './components/UsePessoa.vue';
+import TheHeader from "@/components/form/TheHeader.vue";
 
   export default {
     name: 'App',
     components: {
+      TheHeader,
       PrimeiroComponente,
       LifeCycle,
       Pessoa
-    }}
+    },
+    data(){
+      return {
+        testando: "Teste"
+      }
+    }
+  }
 
 </script>
+
+<style>
+  body {
+    background-color: #333;
+    color: #FFF;
+  }
+  a {
+    color: red;
+  }
+  .teste {
+    background-color: black;
+  }
+
+
+</style>
--- 2_projeto_vue/src/components/UseInfo.vue
+++ 2_projeto_vue/src/components/UseInfo.vue
@@ -13,7 +13,7 @@
             <button @click="showEmail">{{textoBotao}}</button>
         </div>
         <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
-        <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <p class="teste">Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
     <UsePicture />
     </div>
 </template>
@@ -48,3 +48,9 @@ export default{
     }
 }
 </script>
+
+<style>
+  .paragrafo-pai{
+    color: red;
+  }
+</style>
